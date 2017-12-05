8bec9b869bfce0ad2a6148ad6137908bfa69d706
Julien Grossrieder <julien@grossrieder.com>
2017-12-05 06:58:03 +0100

    Initial commit
    Add dev files

---
+++ LGuard.App/App.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using System.Windows.Navigation;
+using Gma.System.MouseKeyHook;
+using Application = System.Windows.Application;
+
+namespace LGuard.App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IKeyboardMouseEvents _globalHook;
+        private bool _lwinDown = false;
+        private bool _rwinDown = false;
+
+        /*
+
+
+        [DllImport("User32.dll")]
+        private static extern bool RegisterHotKey(
+            [In] IntPtr hWnd,
+            [In] int id,
+            [In] uint fsModifiers,
+            [In] uint vk);
+
+        [DllImport("User32.dll")]
+        private static extern bool UnregisterHotKey(
+            [In] IntPtr hWnd,
+            [In] int id);
+
+        private HwndSource _source;
+        private const int HOTKEY_ID = 9000;
+
+        
+
+
+
+        private void RegisterHotKey()
+        {
+            var helper = new WindowInteropHelper(MainWindow);
+            const uint VK_L = 0x4F;// 0x4C;
+            const uint MOD_CTRL = 0x0008;
+            if (!RegisterHotKey(helper.Handle, HOTKEY_ID, MOD_CTRL, VK_L))
+            {
+                // handle error
+            }
+        }
+
+        private void UnregisterHotKey()
+        {
+            var helper = new WindowInteropHelper(MainWindow);
+            UnregisterHotKey(helper.Handle, HOTKEY_ID);
+        }
+
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_HOTKEY = 0x0312;
+            switch (msg)
+            {
+                case WM_HOTKEY:
+                    switch (wParam.ToInt32())
+                    {
+                        case HOTKEY_ID:
+                            OnHotKeyPressed();
+                            handled = true;
+                            break;
+                    }
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
+        private void OnHotKeyPressed()
+        {
+            // do stuff
+        }
+        
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+
+            _source.RemoveHook(HwndHook);
+            _source = null;
+            UnregisterHotKey();
+        }
+        
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow =new MainWindow();
+            MainWindow.Show();
+            MainWindow.Hide();
+            var helper = new WindowInteropHelper(MainWindow);
+            _source = HwndSource.FromHwnd(helper.Handle);
+            _source.AddHook(HwndHook);
+            RegisterHotKey();
+
+        }*/
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            _globalHook = Hook.GlobalEvents();
+            _globalHook.KeyDown += _globalHook_KeyDown;
+            _globalHook.KeyUp += _globalHook_KeyUp;
+        }
+
+        private void _globalHook_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.LWin)
+            {
+                _lwinDown = false;
+            }
+            else if (e.KeyCode == Keys.RWin)
+            {
+                _rwinDown = false;
+            }
+        }
+
+        private void _globalHook_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.LWin)
+            {
+                _lwinDown = true;
+            }
+            else if (e.KeyCode == Keys.RWin)
+            {
+                _rwinDown = true;
+            }
+            else if (e.KeyCode == Keys.L && (_lwinDown || _rwinDown))
+            {
+                e.Handled = true;
+
+            }
+        }
+    }
+}
---
+++ LGuard.App/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LGuard.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<AuxiliaryWindow> _windows = new List<AuxiliaryWindow>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void ShowLock(object sender, RoutedEventArgs e)
+        {
+                myWindow.Top = Screen.PrimaryScreen.WorkingArea.Top;
+                myWindow.Left = Screen.PrimaryScreen.WorkingArea.Left;
+            
+
+            foreach (Screen screen in Screen.AllScreens.Where(s=>!s.Primary))
+            {
+                AuxiliaryWindow auxiliaryWindow = new AuxiliaryWindow()
+                {WindowStartupLocation = WindowStartupLocation.Manual,
+                    Top = screen.WorkingArea.Top,
+                    Left = screen.WorkingArea.Left,
+                    WindowState = WindowState.Normal,
+                    Width = screen.WorkingArea.Width,
+                    Height = screen.WorkingArea.Height
+            };
+                auxiliaryWindow.Loaded += OnAuxiliaryWindowLoaded;
+                _windows.Add(auxiliaryWindow);
+                auxiliaryWindow.Show();
+            }
+            myWindow.Show();
+        }
+
+        private void OnAuxiliaryWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            var senderWindow = sender as Window;
+            senderWindow.WindowState = WindowState.Maximized;
+        }
+
+        private void HideLock(object sender, RoutedEventArgs e)
+        {
+            CloseEverything();
+        }
+
+        private void CloseEverything()
+        {
+            myWindow.Hide();
+            foreach (AuxiliaryWindow auxiliaryWindow in _windows)
+            {
+                auxiliaryWindow.Loaded -= OnAuxiliaryWindowLoaded;
+                auxiliaryWindow.Close();
+            }
+            _windows.Clear();
+        }
+
+        private void OnWindowKeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                CloseEverything();
+            }
+        }
+    }
+}
